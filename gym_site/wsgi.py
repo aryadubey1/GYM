4bfa05a6e6f814960f59cba1306dfe404097ef0f
aryadubey1 <120629785+aryadubey1@users.noreply.github.com>
2026-02-25 21:30:28 +0530

    Setup for Render deployment

--- gym_site/wsgi.py
+++ gym_site/wsgi.py
@@ -10,7 +10,20 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gym_site.settings')
 
 application = get_wsgi_application()
+
+# --- Admin Auto-Creation Hack ---
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Arya@123')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superuser '{username}' created successfully.")
+# --------------------------------
