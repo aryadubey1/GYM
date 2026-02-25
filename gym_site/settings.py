c1e6b6a6bcfedd657bd9ca2ff94873f22f7092af
aryadubey1 <120629785+aryadubey1@users.noreply.github.com>
2026-02-25 19:29:11 +0530

    Updated Database

--- gym_site/settings.py
+++ gym_site/settings.py
@@ -15,6 +15,7 @@ import os
 from pickle import TRUE
 from dotenv import load_dotenv
 load_dotenv()
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,10 +83,10 @@ WSGI_APPLICATION = 'gym_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 
